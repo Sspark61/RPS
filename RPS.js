2a70a49a4b244d78483ff351287bf88da46e2664
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2024-08-05 19:35:09 +0300

    changed the logic of the game and linked it to the ui

--- RPS.js
+++ RPS.js
@@ -1,56 +1,74 @@
+let P;
+let Pscore = 0;
+let Cscore = 0;
+const btn = document.querySelectorAll("button");
+const Pweapon = document.querySelector(".Weapon1");
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id === "rock") {
+      Pweapon.innerHTML = `<img class="pics" src="Resources/hand.png" alt="Rock" />`;
+      P = "rock";
+    } else if (button.id === "paper") {
+      Pweapon.innerHTML = `<img class="pics" src="Resources/hand-paper.png" alt="Paper" />`;
+      P = "paper";
+    } else {
+      Pweapon.innerHTML = `<img class="pics" src="Resources/scissors.png" alt="Scissors" />`;
+      P = "scissors";
+    }
+    Play();
+    check();
+  });
+});
+
+const Cweapon = document.querySelector(".Weapon2");
 function GetCompChoice() {
   let choice = Math.floor(Math.random() * 3);
   if (choice === 0) {
+    Cweapon.innerHTML = `<img class="pics" src="Resources/hand.png" alt="Rock" />`;
     return "rock";
   } else if (choice === 1) {
+    Cweapon.innerHTML = `<img class="pics" src="Resources/hand-paper.png" alt="Paper" />`;
     return "paper";
   } else {
+    Cweapon.innerHTML = `<img class="pics" src="Resources/scissors.png" alt="Scissors" />`;
     return "scissors";
   }
 }
 
-function GetPlayerChoice() {
-  let Choice = prompt("Choose one of the Weapons\n1.Rock\n2.Paper\n3.Scissors");
-  return Choice.toLowerCase();
-}
-
-function FetchWeapons(PlayerChoice, CompChoice) {
-  PlayerChoice = GetPlayerChoice();
-  CompChoice = GetCompChoice();
-  return { PlayerChoice, CompChoice };
-}
-
+const alertMsg = document.querySelector(".msg");
+const theRule = document.querySelector(".rule");
+const pscore = document.querySelector(".pscore");
+const cscore = document.querySelector(".cscore");
 function Play(PlayerChoice, CompChoice) {
-  let Pscore = 0;
-  let Cscore = 0;
-  let P;
-  let C;
-  console.log("Welcome To Rock Paper Scissors");
-  while (Pscore !== 5 && Cscore !== 5) {
-    ({ PlayerChoice, CompChoice } = FetchWeapons(P, C));
-    if (
-      (PlayerChoice === "rock" && CompChoice === "scissors") ||
-      (PlayerChoice === "paper" && CompChoice === "rock") ||
-      (PlayerChoice === "scissors" && CompChoice === "paper")
-    ) {
-      Pscore++;
-      console.log(`Nice One\nSCORE:\nYou: ${Pscore}    Computer: ${Cscore}`);
-    } else if (
-      (CompChoice === "rock" && PlayerChoice === "scissors") ||
-      (CompChoice === "paper" && PlayerChoice === "rock") ||
-      (CompChoice === "scissors" && PlayerChoice === "paper")
-    ) {
-      Cscore++;
-      console.log(`You Idiot\nSCORE\nYou: ${Pscore}    Computer: ${Cscore}`);
-    } else {
-      console.log(`It's a tie\nSCORE\nYou: ${Pscore}    Computer: ${Cscore}`);
-    }
-  }
-  if (Pscore === 5) {
-    console.log("The crazy son of a bitch did it");
+  CompChoice = GetCompChoice();
+  PlayerChoice = P;
+  if (
+    (PlayerChoice === "rock" && CompChoice === "scissors") ||
+    (PlayerChoice === "paper" && CompChoice === "rock") ||
+    (PlayerChoice === "scissors" && CompChoice === "paper")
+  ) {
+    Pscore++;
+    alertMsg.innerHTML = `Nice One`;
+    pscore.innerHTML = `${Pscore}`;
+  } else if (
+    (CompChoice === "rock" && PlayerChoice === "scissors") ||
+    (CompChoice === "paper" && PlayerChoice === "rock") ||
+    (CompChoice === "scissors" && PlayerChoice === "paper")
+  ) {
+    Cscore++;
+    alertMsg.innerHTML = `You Idiot`;
+    cscore.innerHTML = `${Cscore}`;
   } else {
-    console.log("a robot has beaten you ? shame ...");
+    alertMsg.innerHTML = `It's a tie`;
   }
 }
 
-Play();
+function check() {
+  if (Pscore === 5) {
+    alert("The Crazy Son of a bitch did it");
+    location.reload();
+  } else if (Cscore == 5) {
+    alert("a Robot has beaten you ? shame ...");
+    location.reload();
+  }
+}
